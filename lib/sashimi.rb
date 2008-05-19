661b2f9c74c656bf2642805cd56e4d1bbd72fb20
Luca Guidi <guidi.luca@gmail.com>
2008-05-19 10:26:10 +0200

    Fixed lib loading in sashimi.rb

--- lib/sashimi.rb
+++ lib/sashimi.rb
@@ -1,8 +1,11 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require 'uri'
 require 'yaml'
 require 'fileutils'
 require 'rubygems'
 require 'activesupport'
-require 'lib/sashimi/plugin'
-require 'lib/sashimi/repositories'
-require 'lib/sashimi/version'
+require 'sashimi/plugin'
+require 'sashimi/repositories'
+require 'sashimi/version'
