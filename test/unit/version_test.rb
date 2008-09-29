444098152715fd400f4a5328b8bf16db4b96cd59
Luca Guidi <guidi.luca@gmail.com>
2008-09-29 14:33:32 +0200

    Prepare for v0.2.1

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -3,6 +3,6 @@ require 'test/test_helper'
 
 class VersionTest < Test::Unit::TestCase
   def test_version
-    assert_equal '0.2.0', Sashimi::VERSION::STRING
+    assert_equal '0.2.1', Sashimi::VERSION::STRING
   end
 end
